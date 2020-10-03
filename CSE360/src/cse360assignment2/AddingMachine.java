dfcfb9b03505f19b1009883d7c9d6b1c92fab6be
Jason Jimenez <jasonjimenez3975@gmail.com>
2020-10-02 23:15:20 -0700

    Second Version

--- CSE360/src/cse360assignment2/AddingMachine.java
+++ CSE360/src/cse360assignment2/AddingMachine.java
@@ -3,25 +3,44 @@ package cse360assignment2;
 public class AddingMachine {
 
 	 private int total;
-	  
+	 private String totalString;
+	 
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    totalString = "0";
 	  }
 	  
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
 	  public void add (int value) {
+		  total += value;
+		  totalString = totalString + " + " + value;//adding to the string
 	  }
 
 	  public void subtract (int value) {
+		  total -= value;
+		  totalString = totalString + " - " + value;//adding to the string
 	  }
 
 	  public String toString () {
-	    return "";
+	    return totalString;// returning the string as is
 	  }
 
 	  public void clear() {
+		  total = 0;
+		  totalString = "0";
 	  }
+	  public static void main(String[] args) {
+		  //testing
+		  AddingMachine myCalculator = new AddingMachine();
+		  myCalculator.add(4);
+		  myCalculator.subtract(2);
+		  myCalculator.add(5);
+		  System.out.print(myCalculator.toString());
+		  
+		  
+	  }
+	  
 }
